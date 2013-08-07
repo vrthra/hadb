f12243bba4ff5de8f71ffec4c326454de3a972ac
Rahul Gopinath <rahul@puppetlabs.com>
2013-08-07 15:48:18 -0700

    updat

--- pgsql-cluster/etc/site.pp
+++ pgsql-cluster/etc/site.pp
@@ -1,16 +1,14 @@
 node 'vm-pgmaster' {
-#  class { 'pe_postgresql':
-#    node_type => 'master',
-#    address   => 'eth1',
-#    address_type => 'dev',
+#  class { 'pe_postgresql::master':
+#    myid   => 'eth1',
+#    idtype => 'dev',
 #  }
 }
 
 node 'vm-pgslave' {
-#  class { 'pe_postgresql':
-#    node_type => 'slave',
-#    address   => 'eth1',
-#    address_type => 'dev',
+#  class { 'pe_postgresql::slave':
+#    myid   => 'eth1',
+#    idtype => 'dev',
 #  }
 }
 
